c539a0be6aae38d3f369758888df6c070b148abb
Mohit Patil <mohitpatil546@gmail.com>
2016-02-18 11:49:50 +0530

    loginApp is added

---
+++ socolapp/static/login/loginApp.js
@@ -0,0 +1,32 @@
+angular.module('socolappLogin', ['ui.router', 'ui.bootstrap'])
+.config(function($stateProvider, $urlRouterProvider){
+
+	$stateProvider
+
+	.state('home', {
+		url: '/home'
+	})
+	.state('signIn', {
+		url: '/signIn',
+		templateUrl: '/signIn',
+		controller: 'signInController'
+	})
+	.state('signUp', {
+		url: '/signUp',
+		templateUrl: '/signUp',
+		controller: 'signUpController'
+	});
+})
+
+.controller('loginController', function($scope){
+	console.log('loginController is loaded');
+
+})
+.controller('signInController', function($scope){
+	console.log('loginController is loaded');
+
+})
+.controller('signUpController', function($scope){
+	console.log('loginController is loaded');
+
+});
