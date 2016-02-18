f38bd0f9984b0916d5c018ed62a1d78074291813
Akash <acwankhede@gmail.com>
2016-02-18 12:19:11 +0530

    added urls of login module

--- socolapp/socolapp/urls.py
+++ socolapp/socolapp/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from .views import(
+	user_login,
+	)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^user_login/', user_login)
+
 ]
